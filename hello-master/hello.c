38931303c9cc4f784e9904c347e7a0da4590a2bf
Dalmanzazaval <Dalmanzazaval@users.noreply.github.com>
2020-08-22 20:39:11 -0500

    Create hello.c

---
+++ hello-master/hello.c
@@ -0,0 +1,21 @@
+/**
+ * Author: John McGarvey
+ *         jmcgarvey3@huskers.unl.edu
+ * Date: 2020/08/18
+ *
+ * Hello world program our first lab
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("John McGarvey, but I go by Jack!\n");
+  printf("My major is Computer Science!\n");
+
+  printf("Diego Almanza\n");
+  printf("My major is Electrical Engineer\n");
+
+  return 0;
+}
