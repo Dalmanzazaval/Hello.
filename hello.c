38f77d66ff9d69cb17c1261056413e5050e9d392
Dalmanzazaval <Dalmanzazaval@users.noreply.github.com>
2020-08-22 20:18:20 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+*Diego Almanza Zavala
+*2020/08/17
+*
+* A simple hello world program in C
+*	
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main( int argc, char **argv) {
+
+
+printf("Diego Almanza!\n");
+printf("Electrical Engineer\n");
+
+return 0;
+}
